0b9d0c66ff45d0e4398a9b8a576d72ca7beeedbd
Nik McLaughlin <72171383+nikmclaughlin@users.noreply.github.com>
2023-07-07 17:33:27 -0700

    tw mig 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -4,17 +4,19 @@ import MetaTags from "../components/Metatags";
 
 export default function SoBlo404() {
 	return (
-		<main>
+		<main className="py-4 px-[10vw]">
 			<MetaTags
 				title="SoBlo 404"
 				description="Tried to find a different page"
 				image="/travolta-lost.gif"
 				path="/404"
 			/>
-			<h1> 404: Oops! Page not found. </h1>
+			<h1 className="text-4xl m-4"> 404 - Oops! Page not found. </h1>
 			<Image src="/travolta-lost.gif" width={249} height={240} alt=""></Image>
 			<Link href={"/"}>
-				<button className="btn-blue">Take me home</button>
+				<button className="bg-indigo-600 text-white py-4 px-8 rounded-md mx-2 hover:brightness-90">
+					Take me home
+				</button>
 			</Link>
 		</main>
 	);
